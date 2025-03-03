8f18ee0ef3e527a045052782667e0b756dba589c
Benjamin Stewart <benjuhminstewart@gmail.com>
2025-03-03 14:15:49 -0800

    feat: finish day 6 and 7

--- src/day06.zig
+++ src/day06.zig
@@ -6,7 +6,7 @@ const Instant = time.Instant;
 const Timer = time.Timer;
 
 const DAY = 6;
-const IS_TEST = true;
+const IS_TEST = false;
 
 var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
 var gpa = gpa_impl.allocator();
@@ -15,6 +15,7 @@ var alloc: std.mem.Allocator = undefined;
 const Context = struct {
     count: usize,
     lines: [][]const u8,
+    nums: []u4,
 };
 
 fn parse() *Context {
@@ -22,22 +23,66 @@ fn parse() *Context {
     const lines = common.get_lines(DAY, alloc, IS_TEST);
     ctx.lines = lines;
     ctx.count = lines.len;
+    const nums = common.get_ints(u4, lines[0], alloc, ",") catch unreachable;
+    ctx.nums = nums;
 
     return ctx;
 }
 
+fn get_lanternfish(ctx: *Context, days: usize) usize {
+    var states = std.AutoHashMap(u4, usize).init(alloc);
+
+    for (0..9) |i| {
+        const i_u4: u4 = @intCast(i);
+        states.put(i_u4, 0) catch unreachable;
+    }
+
+    for (ctx.nums) |num| {
+        if (!states.contains(num)) {
+            states.put(num, 0) catch unreachable;
+        }
+        states.put(num, states.get(num).? + 1) catch unreachable;
+    }
+
+    var next_states = std.AutoHashMap(u4, usize).init(alloc);
+    for (0..days) |_| {
+        next_states.put(0, states.get(1).?) catch unreachable;
+        next_states.put(1, states.get(2).?) catch unreachable;
+        next_states.put(2, states.get(3).?) catch unreachable;
+        next_states.put(3, states.get(4).?) catch unreachable;
+        next_states.put(4, states.get(5).?) catch unreachable;
+        next_states.put(5, states.get(6).?) catch unreachable;
+        next_states.put(6, states.get(7).?) catch unreachable;
+        next_states.put(7, states.get(8).?) catch unreachable;
+        next_states.put(8, states.get(0).?) catch unreachable;
+
+        if (states.get(0).? > 0) {
+            next_states.put(6, next_states.get(6).? + states.get(0).?) catch unreachable;
+        }
+
+        states = next_states;
+        next_states = std.AutoHashMap(u4, usize).init(alloc);
+    }
+
+    var lanternfish: usize = 0;
+    var it = states.iterator();
+    while (it.next()) |kv| {
+        lanternfish += kv.value_ptr.*;
+    }
+
+    return lanternfish;
+}
+
 fn part1() ![]const u8 {
     const ctx = parse();
-    common.print_lines(ctx.lines, alloc) catch {
-        print("Error parsing lines\n", .{});
-    };
-    return try std.fmt.allocPrint(alloc, "part_1={}", .{0});
+    const lanternfish = get_lanternfish(ctx, 80);
+    return try std.fmt.allocPrint(alloc, "part_1={}", .{lanternfish});
 }
 
 fn part2() ![]const u8 {
-    _ = parse();
-
-    return try std.fmt.allocPrint(alloc, "part_2={}", .{0});
+    const ctx = parse();
+    const lanternfish = get_lanternfish(ctx, 256);
+    return try std.fmt.allocPrint(alloc, "part_2={}", .{lanternfish});
 }
 
 pub fn main() !void {
--- src/day07.zig
+++ src/day07.zig
@@ -6,7 +6,7 @@ const Instant = time.Instant;
 const Timer = time.Timer;
 
 const DAY = 7;
-const IS_TEST = true;
+const IS_TEST = false;
 
 var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
 var gpa = gpa_impl.allocator();
@@ -15,6 +15,7 @@ var alloc: std.mem.Allocator = undefined;
 const Context = struct {
     count: usize,
     lines: [][]const u8,
+    positions: []i64,
 };
 
 fn parse() *Context {
@@ -22,22 +23,69 @@ fn parse() *Context {
     const lines = common.get_lines(DAY, alloc, IS_TEST);
     ctx.lines = lines;
     ctx.count = lines.len;
+    ctx.positions = common.get_ints(i64, lines[0], alloc, ",") catch unreachable;
 
     return ctx;
 }
 
+fn total_fuel(ctx: *Context, alignment: i64) usize {
+    var total: usize = 0;
+    for (ctx.positions) |pos| {
+        total += @abs(pos - alignment);
+    }
+
+    return total;
+}
+
+fn total_fuel_2(ctx: *Context, alignment: i64) usize {
+    var total: usize = 0;
+    for (ctx.positions) |pos| {
+        const n = @abs(pos - alignment);
+        const numerator = n * (n + 1);
+        const denominator = 2;
+        total += @divFloor(numerator, denominator);
+    }
+    return total;
+}
+
 fn part1() ![]const u8 {
     const ctx = parse();
-    common.print_lines(ctx.lines, alloc) catch {
-        print("Error parsing lines\n", .{});
-    };
-    return try std.fmt.allocPrint(alloc, "part_1={}", .{0});
+    var min_fuel: usize = std.math.maxInt(usize);
+    for (ctx.positions) |pos| {
+        min_fuel = @min(min_fuel, total_fuel(ctx, pos));
+    }
+    return try std.fmt.allocPrint(alloc, "part_1={}", .{min_fuel});
 }
 
 fn part2() ![]const u8 {
-    _ = parse();
+    const ctx = parse();
+    var min_fuel: usize = std.math.maxInt(usize);
+    const statistics = stats(ctx);
+    for (statistics.min..statistics.max + 1) |p| {
+        const pos: i64 = @intCast(p);
+        min_fuel = @min(min_fuel, total_fuel_2(ctx, pos));
+    }
+    return try std.fmt.allocPrint(alloc, "part_2={}", .{min_fuel});
+}
 
-    return try std.fmt.allocPrint(alloc, "part_2={}", .{0});
+const Stats = struct {
+    min: usize,
+    max: usize,
+};
+
+fn stats(ctx: *Context) Stats {
+    var min: usize = std.math.maxInt(usize);
+    var max: usize = 0;
+    for (ctx.positions) |p| {
+        const pos: usize = @intCast(p);
+        min = @min(min, pos);
+        max = @max(max, pos);
+    }
+
+    return Stats{
+        .min = min,
+        .max = max,
+    };
 }
 
 pub fn main() !void {
