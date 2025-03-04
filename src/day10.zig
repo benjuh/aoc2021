075d8cc4c0a37f1d9fdfd58d83b61dbace09d9b1
Benjamin Stewart <benjuhminstewart@gmail.com>
2025-03-04 12:21:33 -0800

    feat: finish day 10 and 11

--- src/day10.zig
+++ src/day10.zig
@@ -6,7 +6,7 @@ const Instant = time.Instant;
 const Timer = time.Timer;
 
 const DAY = 10;
-const IS_TEST = true;
+const IS_TEST = false;
 
 var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
 var gpa = gpa_impl.allocator();
@@ -17,27 +17,112 @@ const Context = struct {
     lines: [][]const u8,
 };
 
+pub fn is_open(c: u8) bool {
+    return switch (c) {
+        '(' => true,
+        '[' => true,
+        '{' => true,
+        '<' => true,
+        else => false,
+    };
+}
+
+pub fn get_open(c: u8) u8 {
+    return switch (c) {
+        ')' => '(',
+        ']' => '[',
+        '}' => '{',
+        '>' => '<',
+        else => 0,
+    };
+}
+
+pub fn get_score(c: u8) usize {
+    return switch (c) {
+        ')' => 3,
+        ']' => 57,
+        '}' => 1197,
+        '>' => 25137,
+        else => 0,
+    };
+}
+
+fn first_invalid(line: []const u8) u8 {
+    var stack = std.ArrayList(u8).init(alloc);
+    for (line) |c| {
+        if (is_open(c)) {
+            stack.append(c) catch unreachable;
+        } else {
+            if (stack.items[stack.items.len - 1] != get_open(c)) {
+                return c;
+            } else {
+                _ = stack.pop();
+            }
+        }
+    }
+
+    return 0;
+}
+
 fn parse() *Context {
     var ctx = alloc.create(Context) catch unreachable;
     const lines = common.get_lines(DAY, alloc, IS_TEST);
     ctx.lines = lines;
     ctx.count = lines.len;
-
     return ctx;
+}
+
+fn get_value(c: u8) usize {
+    return switch (c) {
+        '(' => 1,
+        '[' => 2,
+        '{' => 3,
+        '<' => 4,
+        else => 0,
+    };
+}
+
+fn get_completion_score(line: []const u8) usize {
+    var stack = std.ArrayList(u8).init(alloc);
+    var score: usize = 0;
+    for (line) |c| {
+        if (is_open(c)) {
+            stack.append(c) catch unreachable;
+        } else {
+            _ = stack.pop();
+        }
+    }
+    while (stack.items.len > 0) {
+        const c = stack.pop() orelse unreachable;
+        score = (score * 5) + get_value(c);
+    }
+    return score;
 }
 
 fn part1() ![]const u8 {
     const ctx = parse();
-    common.print_lines(ctx.lines, alloc) catch {
-        print("Error parsing lines\n", .{});
-    };
-    return try std.fmt.allocPrint(alloc, "part_1={}", .{0});
+    var sum: usize = 0;
+    for (ctx.lines) |line| {
+        const first = first_invalid(line);
+        sum += get_score(first);
+    }
+    return try std.fmt.allocPrint(alloc, "part_1={}", .{sum});
 }
 
 fn part2() ![]const u8 {
-    _ = parse();
+    const ctx = parse();
+    var scores = std.ArrayList(usize).init(alloc);
+    for (ctx.lines) |line| {
+        if (first_invalid(line) != 0) continue;
+        const score = get_completion_score(line);
+        scores.append(score) catch unreachable;
+    }
+    const sorted = scores.toOwnedSlice() catch unreachable;
+    std.mem.sort(usize, sorted, {}, std.sort.desc(usize));
 
-    return try std.fmt.allocPrint(alloc, "part_2={}", .{0});
+    const res = sorted[sorted.len / 2];
+
+    return try std.fmt.allocPrint(alloc, "part_2={}", .{res});
 }
 
 pub fn main() !void {
--- src/day11.zig
+++ src/day11.zig
@@ -6,38 +6,133 @@ const Instant = time.Instant;
 const Timer = time.Timer;
 
 const DAY = 11;
-const IS_TEST = true;
+const IS_TEST = false;
 
 var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
 var gpa = gpa_impl.allocator();
 var alloc: std.mem.Allocator = undefined;
 
+const adjacents = [_][2]i32{
+    [2]i32{ -1, 0 },
+    [2]i32{ 1, 0 },
+    [2]i32{ 0, -1 },
+    [2]i32{ 0, 1 },
+    [2]i32{ -1, -1 },
+    [2]i32{ 1, -1 },
+    [2]i32{ -1, 1 },
+    [2]i32{ 1, 1 },
+};
+
+const Point = struct {
+    row: usize,
+    col: usize,
+};
+
 const Context = struct {
     count: usize,
-    lines: [][]const u8,
+    octopuses: [10][10]u8,
+    flashes: usize,
+
+    pub fn step(self: *Context) usize {
+        var local_flashes: usize = 0;
+        var queue = std.fifo.LinearFifo(Point, .Dynamic).init(alloc);
+        var visited = std.AutoHashMap(Point, void).init(alloc);
+        for (self.octopuses, 0..) |row, r| {
+            for (row, 0..) |_, c| {
+                self.octopuses[r][c] += 1;
+                if (self.octopuses[r][c] > 9) {
+                    self.octopuses[r][c] = 0;
+                    local_flashes += 1;
+                    queue.writeItem(Point{ .row = r, .col = c }) catch unreachable;
+                    visited.put(Point{ .row = r, .col = c }, {}) catch unreachable;
+                }
+            }
+        }
+
+        while (queue.readItem()) |point| {
+            visited.put(point, {}) catch unreachable;
+            self.flashes += 1;
+            for (adjacents) |adjacent| {
+                const dr = adjacent[0];
+                const dc = adjacent[1];
+                const r: i32 = @intCast(point.row);
+                const c: i32 = @intCast(point.col);
+                const nr = r + dr;
+                const nc = c + dc;
+                if (nr < 0 or nr >= self.octopuses.len or nc < 0 or nc >= self.octopuses[0].len) {
+                    continue;
+                }
+                const new_row: usize = @intCast(nr);
+                const new_col: usize = @intCast(nc);
+                if (visited.contains(Point{ .row = new_row, .col = new_col })) {
+                    continue;
+                }
+                self.octopuses[new_row][new_col] += 1;
+                if (self.octopuses[new_row][new_col] > 9) {
+                    self.octopuses[new_row][new_col] = 0;
+                    visited.put(Point{ .row = new_row, .col = new_col }, {}) catch unreachable;
+                    local_flashes += 1;
+                    queue.writeItem(Point{ .row = new_row, .col = new_col }) catch unreachable;
+                }
+            }
+        }
+
+        return local_flashes;
+    }
+
+    pub fn print_octopuses(self: *const Context) void {
+        print("\n", .{});
+        const green = "\x1b[32m";
+        const reset = "\x1b[0m";
+        for (self.octopuses) |row| {
+            for (row) |cell| {
+                if (cell == 0) {
+                    print("{s}{d}{s}", .{ green, cell, reset });
+                    continue;
+                }
+                print("{d}", .{cell});
+            }
+            print("\n", .{});
+        }
+    }
 };
 
 fn parse() *Context {
     var ctx = alloc.create(Context) catch unreachable;
     const lines = common.get_lines(DAY, alloc, IS_TEST);
-    ctx.lines = lines;
+    var octopuses: [10][10]u8 = undefined;
+    for (lines, 0..) |line, i| {
+        for (line, 0..) |c, j| {
+            octopuses[i][j] = c - '0';
+        }
+    }
+    ctx.octopuses = octopuses;
     ctx.count = lines.len;
+    ctx.flashes = 0;
 
     return ctx;
 }
 
 fn part1() ![]const u8 {
     const ctx = parse();
-    common.print_lines(ctx.lines, alloc) catch {
-        print("Error parsing lines\n", .{});
-    };
-    return try std.fmt.allocPrint(alloc, "part_1={}", .{0});
+    const steps = 100;
+    for (0..steps) |_| {
+        _ = ctx.step();
+    }
+    return try std.fmt.allocPrint(alloc, "part_1={}", .{ctx.flashes});
 }
 
 fn part2() ![]const u8 {
-    _ = parse();
-
-    return try std.fmt.allocPrint(alloc, "part_2={}", .{0});
+    const ctx = parse();
+    var i: usize = 0;
+    while (true) {
+        i += 1;
+        const flashes = ctx.step();
+        if (flashes == 100) {
+            break;
+        }
+    }
+    return try std.fmt.allocPrint(alloc, "part_2={}", .{i});
 }
 
 pub fn main() !void {
