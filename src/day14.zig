90dfa958c345c0263dce178564b7771963f3e817
Benjamin Stewart <benjuhminstewart@gmail.com>
2025-03-07 11:35:51 -0800

    feat: finish day 14 and 15

--- src/day14.zig
+++ src/day14.zig
@@ -6,38 +6,108 @@ const Instant = time.Instant;
 const Timer = time.Timer;
 
 const DAY = 14;
-const IS_TEST = true;
+const IS_TEST = false;
 
 var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
 var gpa = gpa_impl.allocator();
 var alloc: std.mem.Allocator = undefined;
 
 const Context = struct {
-    count: usize,
-    lines: [][]const u8,
+    template: []const u8,
+    rules: std.StringHashMap(u8),
+    initial_pair_counts: std.StringHashMap(usize),
+};
+
+const Step = struct {
+    polymer: []const u8,
+    freq_count: [26]usize,
 };
 
 fn parse() *Context {
     var ctx = alloc.create(Context) catch unreachable;
     const lines = common.get_lines(DAY, alloc, IS_TEST);
-    ctx.lines = lines;
-    ctx.count = lines.len;
-
+    var rules = std.StringHashMap(u8).init(alloc);
+    const template = lines[0];
+    for (lines[2..]) |line| {
+        var parts = std.mem.splitSequence(u8, line, " -> ");
+        const key = parts.next().?;
+        const value = parts.next().?[0];
+        rules.put(key, value) catch unreachable;
+    }
+    ctx.template = template;
+    ctx.rules = rules;
+    ctx.initial_pair_counts = get_pair_counts(rules, template);
     return ctx;
+}
+
+fn get_pair_counts(rules: std.StringHashMap(u8), template: []const u8) std.StringHashMap(usize) {
+    var pair_counts = std.StringHashMap(usize).init(alloc);
+    var it = rules.iterator();
+    while (it.next()) |entry| {
+        const key = entry.key_ptr.*;
+        pair_counts.put(key, 0) catch unreachable;
+    }
+    if (template.len == 0) return pair_counts;
+    for (0..template.len - 1) |i| {
+        const pair = std.fmt.allocPrint(alloc, "{c}{c}", .{ template[i], template[i + 1] }) catch unreachable;
+        pair_counts.put(pair, pair_counts.get(pair).? + 1) catch unreachable;
+    }
+
+    return pair_counts;
+}
+
+fn get_polymer(pair_counts: std.StringHashMap(usize), letterCounts: *[26]usize, ctx: *Context) std.StringHashMap(usize) {
+    var new_pair_counts = get_pair_counts(ctx.rules, "");
+
+    var it = pair_counts.iterator();
+    while (it.next()) |entry| {
+        const pair = entry.key_ptr.*;
+        const count = entry.value_ptr.*;
+        const middle = ctx.rules.get(pair).?;
+        const np1 = std.fmt.allocPrint(alloc, "{c}{c}", .{ pair[0], middle }) catch unreachable;
+        const np2 = std.fmt.allocPrint(alloc, "{c}{c}", .{ middle, pair[1] }) catch unreachable;
+        new_pair_counts.put(np1, new_pair_counts.get(np1).? + count) catch unreachable;
+        new_pair_counts.put(np2, new_pair_counts.get(np2).? + count) catch unreachable;
+        letterCounts.*[middle - 'A'] += count;
+    }
+
+    return new_pair_counts;
+}
+
+fn get_result(freq_count: [26]usize) usize {
+    var max: usize = 0;
+    var min: usize = std.math.maxInt(usize);
+    for (freq_count) |count| {
+        if (count > max) {
+            max = count;
+        }
+        if (count < min and count > 0) {
+            min = count;
+        }
+    }
+    return max - min;
 }
 
 fn part1() ![]const u8 {
     const ctx = parse();
-    common.print_lines(ctx.lines, alloc) catch {
-        print("Error parsing lines\n", .{});
-    };
-    return try std.fmt.allocPrint(alloc, "part_1={}", .{0});
+    var pair_counts = ctx.initial_pair_counts;
+    var freq_count: [26]usize = @splat(0);
+    for (0..10) |_| {
+        pair_counts = get_polymer(pair_counts, &freq_count, ctx);
+    }
+    const result = get_result(freq_count);
+    return try std.fmt.allocPrint(alloc, "part_1={}", .{result + 1});
 }
 
 fn part2() ![]const u8 {
-    _ = parse();
-
-    return try std.fmt.allocPrint(alloc, "part_2={}", .{0});
+    const ctx = parse();
+    var pair_counts = ctx.initial_pair_counts;
+    var freq_count: [26]usize = @splat(0);
+    for (0..40) |_| {
+        pair_counts = get_polymer(pair_counts, &freq_count, ctx);
+    }
+    const result = get_result(freq_count);
+    return try std.fmt.allocPrint(alloc, "part_2={}", .{result + 1});
 }
 
 pub fn main() !void {
--- src/day15.zig
+++ src/day15.zig
@@ -6,7 +6,7 @@ const Instant = time.Instant;
 const Timer = time.Timer;
 
 const DAY = 15;
-const IS_TEST = true;
+const IS_TEST = false;
 
 var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
 var gpa = gpa_impl.allocator();
@@ -14,30 +14,147 @@ var alloc: std.mem.Allocator = undefined;
 
 const Context = struct {
     count: usize,
-    lines: [][]const u8,
+    lines: [][]u8,
+    start: Point,
+    end: Point,
 };
 
 fn parse() *Context {
     var ctx = alloc.create(Context) catch unreachable;
     const lines = common.get_lines(DAY, alloc, IS_TEST);
-    ctx.lines = lines;
+    var new_lines = alloc.alloc([]u8, lines.len) catch unreachable;
+    for (new_lines) |*row| {
+        row.* = alloc.alloc(u8, lines[0].len) catch unreachable;
+    }
+    for (new_lines, 0..) |row, y| {
+        for (row, 0..) |_, x| {
+            new_lines[y][x] = lines[y][x];
+        }
+    }
+    ctx.lines = new_lines;
     ctx.count = lines.len;
-
+    ctx.start = Point{ .x = 0, .y = 0 };
+    ctx.end = Point{ .x = @intCast(ctx.count - 1), .y = @intCast(ctx.count - 1) };
     return ctx;
+}
+
+fn parse_int(char: u8) usize {
+    return char - '0';
+}
+
+const directions = [4][2]i32{
+    [2]i32{ 0, 1 },
+    [2]i32{ 0, -1 },
+    [2]i32{ 1, 0 },
+    [2]i32{ -1, 0 },
+};
+
+const Point = struct {
+    x: i32,
+    y: i32,
+};
+
+const Path = struct {
+    risk: usize,
+    node: Point,
+};
+
+fn pathfind(ctx: *Context, grid: [][]u8) usize {
+    var visited = std.AutoHashMap(Point, void).init(alloc);
+    var pq = std.PriorityQueue(Path, void, less_than).init(alloc, {});
+    const start_risk = parse_int(grid[@intCast(ctx.start.y)][@intCast(ctx.start.x)]);
+
+    pq.add(Path{ .risk = start_risk, .node = ctx.start }) catch unreachable;
+
+    while (pq.count() > 0) {
+        const path = pq.remove();
+        const node = path.node;
+        const risk = path.risk;
+        if (node.x == ctx.end.x and node.y == ctx.end.y) {
+            return risk - start_risk;
+        }
+
+        if (visited.contains(node)) {
+            continue;
+        }
+        for (directions) |dir| {
+            const dx = dir[0];
+            const dy = dir[1];
+            const new_x = node.x + dx;
+            const new_y = node.y + dy;
+            if (new_x >= grid[0].len or new_x < 0 or new_y >= grid.len or new_y < 0) {
+                continue;
+            }
+            const xx: usize = @intCast(new_x);
+            const yy: usize = @intCast(new_y);
+            const next_risk = risk + parse_int(grid[yy][xx]);
+            const next = Point{ .x = new_x, .y = new_y };
+            pq.add(Path{ .risk = next_risk, .node = next }) catch unreachable;
+        }
+        visited.put(node, {}) catch unreachable;
+    }
+
+    return std.math.maxInt(usize);
+}
+
+fn less_than(context: void, a: Path, b: Path) std.math.Order {
+    _ = context;
+    return std.math.order(a.risk, b.risk);
 }
 
 fn part1() ![]const u8 {
     const ctx = parse();
-    common.print_lines(ctx.lines, alloc) catch {
-        print("Error parsing lines\n", .{});
-    };
-    return try std.fmt.allocPrint(alloc, "part_1={}", .{0});
+    const res = pathfind(ctx, ctx.lines);
+    return try std.fmt.allocPrint(alloc, "part_1={}", .{res});
 }
 
 fn part2() ![]const u8 {
-    _ = parse();
+    const ctx = parse();
+    const new_grid = try get_bigger_grid(ctx.lines, 5);
+    ctx.end = Point{ .x = @intCast(new_grid[0].len - 1), .y = @intCast(new_grid.len - 1) };
+    const res = pathfind(ctx, new_grid);
+    return try std.fmt.allocPrint(alloc, "part_2={}", .{res});
+}
 
-    return try std.fmt.allocPrint(alloc, "part_2={}", .{0});
+fn get_bigger_grid(grid: [][]u8, multiplier: usize) ![][]u8 {
+    var new_grid = try alloc.alloc([]u8, grid.len * multiplier);
+    for (new_grid) |*row| {
+        row.* = try alloc.alloc(u8, grid[0].len * multiplier);
+    }
+
+    for (new_grid, 0..) |row, y| {
+        for (row, 0..) |_, x| {
+            if (x < grid.len and y < grid[0].len) {
+                new_grid[y][x] = grid[y][x];
+            } else if (x < grid.len) {
+                const old_index = (y % grid.len);
+                var new_risk: u8 = @intCast((((grid[old_index][x] - '0') + (y / grid.len))) % 9);
+                if (new_risk == 0) {
+                    new_risk = 9;
+                }
+                new_grid[y][x] = new_risk + '0';
+            } else if (y < grid[0].len) {
+                const old_index = (x % grid[0].len);
+                var new_risk: u8 = @intCast((((grid[y][old_index] - '0') + (x / grid[0].len)) % 9));
+                if (new_risk == 0) {
+                    new_risk = 9;
+                }
+                new_grid[y][x] = new_risk + '0';
+            } else {
+                const old_x = x % grid[0].len;
+                const old_y = y % grid.len;
+                const additive_y: u8 = @intCast(x / grid[0].len);
+                const additive_x: u8 = @intCast(y / grid.len);
+                var new_risk: u8 = @intCast((((grid[old_y][old_x] - '0') + (additive_y + additive_x)) % 9));
+                if (new_risk == 0) {
+                    new_risk = 9;
+                }
+                new_grid[y][x] = new_risk + '0';
+            }
+        }
+    }
+
+    return new_grid;
 }
 
 pub fn main() !void {
